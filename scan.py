f3cb11dca7860b38788290b86361c3fa568231cf
Elysia Quah <elysia.quah@nlb-int-svc-proxy-06a01ef5ff9ffd47.elb.us-gov-west-1.amazonaws.com>
2024-07-03 14:50:39 -0400

    restructured scan.py using click

--- scan.py
+++ scan.py
@@ -1,78 +1,77 @@
+import click
 import subprocess
 import json
 import sys
 import os
 from prettytable import PrettyTable
 
-def run_semgrep(target_dir, rules_file, options):
-    result = subprocess.run(
-        ['semgrep', 'scan', '--config', rules_file, '--json', target_dir],
-        capture_output=True,
-        text=True
-    )
-    
-    if result.returncode != 0:
-        print(f"Semgrep failed with return code {result.returncode}")
-        print(result.stderr)
-        return
-    
-    try:
-        output = json.loads(result.stdout)
-    except json.JSONDecodeError as e:
-        print(f"Failed to parse JSON output: {e}")
-        return
-    
-    files = {result['path'] for result in output['results']}
-    
+def run_semgrep(target_dir, rules):
+
+    total_output = {}
+
+    # should implement this better
+    if target_dir == 'code/false-examples/':
+        test = 'code/false-examples/*/**'
+    else:
+        test = target_dir
+
+    for rule in rules:
+        command = 'semgrep scan --config ' + rule + ' --json ' + test
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True,
+            shell=True
+        )
+
+        if result.returncode != 0:
+            print(f"Semgrep failed with return code {result.returncode} with command semgrep scan --config {rule} --json {test}\n{result.stderr}")
+            sys.exit(1)
+
+        try:
+            output = json.loads(result.stdout)
+        except json.JSONDecodeError as e:
+            print(f"Failed to parse JSON output: {e}")
+            sys.exit(1)
+        
+        total_output.update(output)
+
+    # might be nice to also print number of files scanned
+    # print(total_output['paths']['scanned'])
+
+    files = {result['path'] for result in total_output['results']}
+    files_detected = {path for path in files if path.startswith(target_dir)} 
+    total = len(os.listdir(target_dir))
+    rate = len(files_detected)/total * 100
+    return [len(files_detected), rate]
+
+@click.command()
+@click.option('--true-examples', type=click.Path(exists=True), default=None, help='path to true examples')
+@click.option('--false-examples', type=click.Path(exists=True), default=None, help='path to false examples')
+@click.option('--rules', multiple=True, type=click.Path(exists=True), default=None, help='path to rules')
+def main(true_examples, false_examples, rules):
+
     columns = ['', 'True', 'False']
     myTable = PrettyTable()
     myTable.add_column(columns[0], ['Number of files detected', 'Detection rate'])
 
-    if options[0] == 0 or options[0] == 1:
-        true_files_detected = {path for path in files if path.startswith('code/true-examples-malicious/')}
-        true_total = len(os.listdir('code/true-examples-malicious/'))
-        true_rate = len(true_files_detected)/true_total * 100
-        myTable.add_column(columns[1], [len(true_files_detected), str(true_rate) + '%'])
+    if not true_examples and not false_examples:
+        true_examples = 'code/true-examples-malicious/'
+        false_examples = 'code/false-examples/'
+    if not rules:
+        rules = ['rules/']
 
-    if options[0] == 0 or options[0] == 2:
-        false_files_detected = {path for path in files if path.startswith('code/false-examples/')}
-        false_total = len(os.listdir('code/false-examples/'))
-        false_positives = len(false_files_detected)/false_total
-        false_rate = (1 - false_positives) * 100
-        myTable.add_column(columns[2], [len(false_files_detected), str(false_rate) + '%'])
-        print('False positive rate:' + str(false_positives*100) + '%')
-
+    if true_examples:
+        true_result = run_semgrep(true_examples, rules)
+        true_result[1] = str(true_result[1]) + '%'
+        myTable.add_column(columns[1], true_result)
+    if false_examples:
+        false_result = run_semgrep(false_examples, rules)
+        false_result[1] = str(false_result[1]) + '%'
+        myTable.add_column(columns[2], false_result)
+        print('False positive rate: ' + false_result[1])
+    
     print(myTable)
-
-def usage():
-    print("Usage: python scan.py <true/false/all> <specific-rule/all>")
-    sys.exit(1)
-
-def main():
-
-    target_dir = 'code/'
-    rules_file = 'rules/'
-    options = [0, 0]
-
-    if len(sys.argv) == 3:
-        if sys.argv[1] == 'true':
-            target_dir += 'true-examples-malicious'
-            options[0] += 1
-        elif sys.argv[1] == 'false':
-            target_dir += 'false-examples'
-            options[0] += 2 
-        elif sys.argv[1] != 'all':
-            usage()
-        
-        if os.path.exists(rules_file + sys.argv[2]):
-            rules_file += sys.argv[2]
-            options[1] += 1
-        elif sys.argv[2] != 'all':
-            usage()
-    elif len(sys.argv) != 1:
-        usage()
-
-    run_semgrep(target_dir, rules_file, options)
 
 if __name__ == '__main__':
     main()
