09bec923c99e4536c8018f580f1824418556d87e
Elysia Quah <elysia.quah@datadoghq.com>
2024-07-18 17:26:37 -0400

    tag feature added and working

--- scan.py
+++ scan.py
@@ -11,11 +11,12 @@ from prettytable import PrettyTable
 
 
 class Scanner:
-    def __init__(self, target_dir: str, rules: Tuple[str, ...]) -> None:
+    def __init__(self, target_dir: str, rules: Tuple[str, ...], tags: Tuple[str, ...]) -> None:
         self.target_dir = target_dir
         self.rules = rules
         self.total_output = {"results": [], "errors": [], "paths": {"scanned": []}}
         self.batch_size = 5000
+        self.tags = tags
 
     def run(self) -> List[int]:
         files = self.find_files()
@@ -30,7 +31,7 @@ class Scanner:
             return
 
         batch_str = " ".join(shlex.quote(file) for file in batch)
-        command = f'semgrep scan --config {rule} --json {batch_str}'
+        command = f"semgrep scan --config {rule} --json {batch_str}"
         result = subprocess.run(command, capture_output=True, text=True, shell=True)
 
         if result.returncode != 0:
@@ -44,18 +45,29 @@ class Scanner:
             print(f"Failed to parse JSON output: {e}")
             sys.exit(1)
 
-        self.total_output["results"].extend(output.get("results", []))
+        filtered_results = self.filter_results(output.get("results", []))
+        self.total_output["results"].extend(filtered_results)
         self.total_output["errors"].extend(output.get("errors", []))
         self.total_output["paths"]["scanned"].extend(output.get("paths", {}).get("scanned", []))
 
+    def filter_results(self, results: List[Dict]) -> List[Dict]:
+        filtered_results = []
+        for result in results:
+            if "tags" in result.get("extra", {}).get("metadata", {}):
+                tags = result["extra"]["metadata"]["tags"]
+                if any(tag in tags for tag in self.tags):
+                    filtered_results.append(result)
+        return filtered_results
+
     def find_files(self) -> List[str]:
-        pattern = os.path.join(self.target_dir, '**', '*.php')
+        pattern = os.path.join(self.target_dir, "**", "*.php")
         file_list = glob.glob(pattern, recursive=True)
         return file_list
 
     def calculate_results(self) -> List[int]:
         files_detected = len(set(result["path"] for result in self.total_output["results"]))
         files_scanned = len(set(self.total_output["paths"]["scanned"]))
+        # print(set(result["path"] for result in self.total_output["results"]))
         if files_scanned:
             rate = files_detected / files_scanned * 100
         else:
@@ -65,7 +77,13 @@ class Scanner:
 
 
 class CLI:
-    def __init__(self, true_examples: str, false_examples: str, rules: Tuple[str, ...]) -> None:
+    def __init__(
+        self,
+        true_examples: str,
+        false_examples: str,
+        rules: Tuple[str, ...],
+        tags: Tuple[str, ...],
+    ) -> None:
         self.true_examples = true_examples
         self.false_examples = false_examples
 
@@ -74,14 +92,15 @@ class CLI:
             self.true_examples = "tests/true-examples/"
             self.false_examples = "tests/false-examples/"
 
-        self.rules = rules or ("rules/", )
+        self.rules = rules
         self.results = {}
+        self.tags = tags
 
     def run(self) -> None:
         if self.true_examples:
-            self.results["true"] = Scanner(self.true_examples, self.rules).run()
+            self.results["true"] = Scanner(self.true_examples, self.rules, self.tags).run()
         if self.false_examples:
-            self.results["false"] = Scanner(self.false_examples, self.rules).run()
+            self.results["false"] = Scanner(self.false_examples, self.rules, self.tags).run()
 
         output_table = self.generate_table(self.results)
         print(output_table)
@@ -101,9 +120,15 @@ class CLI:
 @click.command()
 @click.option("--true-examples", type=click.Path(exists=True), help="path to true examples")
 @click.option("--false-examples", type=click.Path(exists=True), help="path to false examples")
-@click.option("--rules", multiple=True, type=click.Path(exists=True), default=["rules/"], help="path to rules")
-def main(true_examples: str, false_examples: str, rules: Tuple[str, ...]) -> None:
-    cli = CLI(true_examples, false_examples, rules)
+@click.option("--rules", multiple=True, type=click.Path(exists=True), default=("rules/",), help="path to rules")
+@click.option("--tags", multiple=True, default=["LOW", "MEDIUM", "HIGH"], help="tag options LOW/MEDIUM/HIGH")
+def main(
+    true_examples: str,
+    false_examples: str,
+    rules: Tuple[str, ...],
+    tags: Tuple[str, ...],
+) -> None:
+    cli = CLI(true_examples, false_examples, rules, tags)
     cli.run()
 
 
